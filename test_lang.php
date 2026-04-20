3875729e43bc6817674a712278a27a415803c7ae
harisonfaniry2 <harisonfaniry2@gmail.com>
2026-04-20 13:49:47 +0300

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,13 +1,1 @@
-<?php
-$fichier = "index.php";
-
-if (file_exists($fichier) && is_readable($fichier)) {
-    $handle = fopen($fichier, "r");
-    $contenu = fread($handle, filesize($fichier));
-    fclose($handle);
-
-    echo $contenu;
-} else {
-    echo "Fichier introuvable ou non lisible.";
-}
-?>
+<?php echo file_get_contents('index.php'); ?>
